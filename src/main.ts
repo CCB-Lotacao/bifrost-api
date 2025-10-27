97fa5f21697b7a9e047d796cbb5bc66c28037109
enzosoft <enzo.leandrini@sou.unifeob.edu.br>
2025-10-27 19:09:12 -0300

    adicionado o cors para funcionar o frontend

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const { APP_PORT = 3000 } = process.env;
   await app.listen(APP_PORT, () => {
     Logger.log(
