ed855175fb808cb2d343de2b8dffdfc06a527204
enzosoft <enzo.leandrini@sou.unifeob.edu.br>
2025-11-15 23:13:51 -0300

    added config with UI swagger

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,24 @@ import { Logger } from "@nestjs/common";
 
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle("CCB Lotação")
+    .setVersion("1.0")
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup("/", app, document);
+
   const { APP_PORT = 3000 } = process.env;
+
   await app.listen(APP_PORT, () => {
     Logger.log(
       `Nest application listening on http://localhost:${APP_PORT} 🚀`,
